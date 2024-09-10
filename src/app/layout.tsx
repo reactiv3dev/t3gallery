a3859a55f9d73f321adab087a858219b02ac2175
reactiv3dev <reactivedeveloper@protonmail.com>
2024-09-10 21:12:57 +0200

    database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 function TopNav(){
   return (
-    <nav className="w-full h-14 fixed flex flex-row items-center justify-between bg-[#2e026d] text-white text-xl font-semibold">
+    <nav className="w-full h-14 fixed flex flex-row items-center justify-between bg-[#2e026d] text-white text-xl font-semibold p-5">
       <h1>T3Gallery</h1>
       <div>Welcome</div>
       <div>SignIn</div>
